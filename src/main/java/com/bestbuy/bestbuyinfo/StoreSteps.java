236c700d88fe3e7a202eeb9ee2b79aabcbb9c64c
dharamit9291 <dharamit9291@gmail.com>
2023-01-04 22:02:55 +0000

    dhara serenity p1

---
+++ src/main/java/com/bestbuy/bestbuyinfo/StoreSteps.java
@@ -0,0 +1,7 @@
+package com.bestbuy.bestbuyinfo;
+
+
+public class StoreSteps {
+
+
+}
---
+++ src/main/java/com/bestbuy/constants/EndPoints.java
@@ -0,0 +1,31 @@
+package com.bestbuy.constants;
+
+/**
+ * Created by Jay
+ */
+public class EndPoints {
+
+    /**
+     * This is Endpoints of products
+     */
+//    public static final String GET_ALL_STUDENT = "/list";
+    public static final String GET_SINGLE_PRODUCT_BY_ID = "/products/{productID}";
+    public static final String GET_ALL_PRODUCTS = "/products";
+    public static final String UPDATE_PRODUCT_BY_ID = "/products/{productID}";
+    public static final String DELETE_PRODUCT_BY_ID = "/products/{productID}";
+
+    /**
+     * This is Endpoints of stores
+     */
+//    public static final String GET_ALL_STUDENT = "/list";
+    public static final String GET_SINGLE_STORE_BY_ID = "/stores/{storesID}";
+    public static final String UPDATE_STORE_BY_ID = "/stores/{storesID}";
+    public static final String DELETE_STORE_BY_ID = "/stores/{storesID}";
+
+
+
+    /**
+     * This is Endpoints of Authentication api
+     */
+
+}
